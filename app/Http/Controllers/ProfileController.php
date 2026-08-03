6e1f58cc5b21059b239441c37d7b9f6aa7e1fdeb
Deekshya Sapkota <deekshyasapkota255@gmail.com>
2026-08-03 14:13:32 +0545

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -171,7 +171,6 @@ public function findMatches()
 
             return count($teachMatch) > 0 || count($learnMatch) > 0;
         });
-        dd ($myTeach, $myLearn, $matches);
 
     return view('matches', compact('matches'));
 }
